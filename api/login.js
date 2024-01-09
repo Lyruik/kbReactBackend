687408d2586575844ac5df934965ae537b2569e6
Michael Wadsworth <Michael.Wadsworth@audioenhancement.com>
2024-01-09 09:52:33 -0700

    had to export login router, pushing to get fresh slate

--- api/login.js
+++ api/login.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const loginRouter = express.Router();
 
-loginRouter.post("/", async(req, res, next));
+loginRouter.post("/", async (req, res, next) => {
+  return true;
+});
+
+module.exports = loginRouter;
